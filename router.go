89f05d40f006a96062ed0c1b85a8943124d8f872
Andres Barreto <andres.barreto@koombea.com>
2021-10-12 20:49:28 -0500

    Server created

---
+++ router.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+)
+
+type Router struct {
+	rules map[string]http.HandlerFunc
+}
+
+func NewRouter() *Router {
+	return &Router{
+		rules: make(map[string]http.HandlerFunc),
+	}
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
+	handler, done := r.FindHandler(request.URL.Path)
+
+}
+
+func (r *Router) FindHandler(path string) (http.HandlerFunc, bool) {
+	handler, done := r.rules[path]
+	return handler, done
+}
