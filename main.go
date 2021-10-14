f5507ebeb965dbf652c9bb004bdd8800a9012073
Andres Barreto <andres.barreto@koombea.com>
2021-10-13 19:03:10 -0500

    implement methods

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
 	server := NewServer(":3000")
-	server.Handle("/", HandleRoot)
-	server.Handle("/api", server.AddMiddleware(HandleAPI, CheckAuth(), Logging()))
+	server.Handle("GET", "/", HandleRoot)
+	server.Handle("GET", "/api", server.AddMiddleware(HandleAPI, CheckAuth(), Logging()))
+	server.Handle("POST", "/api", server.AddMiddleware(HandleAPI, CheckAuth(), Logging()))
 	server.Listen()
 }
--- router.go
+++ router.go
@@ -5,25 +5,30 @@ import (
 )
 
 type Router struct {
-	rules map[string]http.HandlerFunc
+	rules map[string]map[string]http.HandlerFunc
 }
 
 func NewRouter() *Router {
 	return &Router{
-		rules: make(map[string]http.HandlerFunc),
+		rules: make(map[string]map[string]http.HandlerFunc),
 	}
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	handler, done := r.FindHandler(request.URL.Path)
-	if !done {
+	handler, pathExists, methodExists := r.FindHandler(request.URL.Path, request.Method)
+	if !pathExists {
 		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if !methodExists {
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 	handler(w, request)
 }
 
-func (r *Router) FindHandler(path string) (http.HandlerFunc, bool) {
-	handler, done := r.rules[path]
-	return handler, done
+func (r *Router) FindHandler(path string, method string) (http.HandlerFunc, bool, bool) {
+	_, pathExists := r.rules[path]
+	handler, methodExists := r.rules[path][method]
+	return handler, pathExists, methodExists
 }
