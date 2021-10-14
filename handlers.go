c8f78a217e12923dc24debdf00006bface8f726f
Andres Barreto <andres.barreto@koombea.com>
2021-10-13 19:37:22 -0500

    json manage

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,39 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandleAPI(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from API!")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metadata MetaData
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "Data: %v\n", metadata)
+}
+
+func HandlePostUser(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	fmt.Println("User name: ", user.Name)
+	fmt.Println("User email: ", user.Email)
+	fmt.Println("User phone: ", user.Phone)
+
+	response, err := user.ToJson()
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
--- main.go
+++ main.go
@@ -5,5 +5,7 @@ func main() {
 	server.Handle("GET", "/", HandleRoot)
 	server.Handle("GET", "/api", server.AddMiddleware(HandleAPI, CheckAuth(), Logging()))
 	server.Handle("POST", "/api", server.AddMiddleware(HandleAPI, CheckAuth(), Logging()))
+	server.Handle("POST", "/create", server.AddMiddleware(PostRequest, CheckAuth(), Logging()))
+	server.Handle("POST", "/user", server.AddMiddleware(HandlePostUser, CheckAuth(), Logging()))
 	server.Listen()
 }
